0231072004bb34bd88f17b53034262c987871627
Cobular <22972550+Cobular@users.noreply.github.com>
2021-02-01 11:22:06 -0800

    Added dotfiles from desktop

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+
+
+# Wasienv
+export WASIENV_DIR="/home/cobular/.wasienv"
+[ -s "$WASIENV_DIR/wasienv.sh" ]; and source "$WASIENV_DIR/wasienv.sh"
+
+# Wasmer
+export WASMER_DIR="/home/cobular/.wasmer"
+[ -s "$WASMER_DIR/wasmer.sh" ]; and source "$WASMER_DIR/wasmer.sh"
