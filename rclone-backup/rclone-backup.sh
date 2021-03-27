0d0695b8e5b30ed871ecf82e8b8ccec64e4f38ab
Cobular <22972550+Cobular@users.noreply.github.com>
2021-03-27 01:52:34 -0700

    fixed issues with rclone backup scripts

--- rclone-backup/rclone-backup.sh
+++ rclone-backup/rclone-backup.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-pip freeze > python-packages.txt
-dpkg --get-selections > apt-packages.lst
-rclone copy --dry-run -vv / b2:cobular --filter-from ./rclone-backup-filter.txt --log-file=rclone-log-$(date +%Y%m%d%H%M%S).log
+pip freeze > /home/cobular/rclone-backup/python-packages.txt
+dpkg --get-selections > /home/cobular/rclone-backup/apt-packages.lst
+sudo rclone copy -v / b2:cobular --filter-from /home/cobular/rclone-backup/rclone-backup-filter.txt --log-file=/home/cobular/rclone-backup/rclone-log-$(date +%Y%m%d%H%M%S).log --skip-links
