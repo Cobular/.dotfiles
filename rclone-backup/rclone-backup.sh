7af81734fd8bac8f9ed5319fda0904998bf2a18a
Cobular <22972550+Cobular@users.noreply.github.com>
2022-12-01 11:25:30 -0800

    added a bunch of exclusions

--- rclone-backup/rclone-backup.sh
+++ rclone-backup/rclone-backup.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 pip freeze > /home/cobular/rclone-backup/python-packages.txt
 dpkg --get-selections > /home/cobular/rclone-backup/apt-packages.lst
-sudo rclone copy -v / b2:cobular --filter-from /home/cobular/rclone-backup/rclone-backup-filter.txt --log-file=/home/cobular/rclone-backup/rclone-log-$(date +%Y%m%d%H%M%S).log --skip-links
+sudo rclone copy -v / b2:cobular --filter-from /home/cobular/rclone-backup/rclone-backup-filter.txt --log-file=/home/cobular/rclone-backup/rclone-log-$(date +%Y%m%d%H%M%S).log --skip-links  --fast-list
